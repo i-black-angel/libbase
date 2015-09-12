bb2bb74836f15351bce83a5703417098c40e8f7e
i-black-angel <iblackangel@163.com>
2015-09-12 17:26:27 +0800

    win32

---
+++ src/win32/signalhandler.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <cstdio>
+#include "signalhandler.h"
+
+using namespace std;
+
+bool base::SignalHandler::handle() {
+	return true;
+}
+
+void base::SignalHandler::run() {
+	/* what you need to do, just imagine this function is an new main() */
+}

38eb35fa86f0b9e27d5b8ce980ec2ffeaf05f520
i-black-angel <iblackangel@163.com>
2015-12-02 10:11:22 +0800

    add win32 process code

--- src/bprocess.cpp
+++ src/bprocess.cpp
@@ -16,11 +16,29 @@ namespace base {
 	}
 
 	uint64_t Process::processid() const {
+#ifdef _MSC_VER
+		return (uint64_t)_getpid();
+#else
 		return (uint64_t)getpid();
-	}
+#endif
+}
 
 	std::string Process::program() const {
 		std::string res;
+#ifdef _MSC_VER
+		char buffer[MAX_PATH] = {0};
+		GetModuleFileName(NULL, buffer, sizeof(buffer));
+
+		char   drive[_MAX_DRIVE]; 
+		char   szdir[_MAX_DIR]; 
+		char   fname[_MAX_FNAME]; 
+		char   ext[_MAX_EXT]; 
+
+		if (0 == _splitpath_s(buffer, drive, szdir, fname, ext)) {
+			res = fname;
+			res += ext;
+		}
+#else
 		const char *pathname = "/proc/self/exe";
 		char buf[512] = {0};
 		ssize_t buflen = readlink(pathname, buf, sizeof(buf));
@@ -39,7 +57,7 @@ namespace base {
 		while ((p = strtok(NULL, delim))) {
 			res = p;
 		}
-		
+#endif		
 		return res;
 	}
 }
